89b615f50d7cdd9fc77cdfdb408de2a2f5be119c
fradam95 <fllradam@gmail.com>
2024-11-19 17:06:43 +0100

    Add project files.

---
+++ GymGo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GymGo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GymGo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GymGo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
